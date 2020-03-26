119704659b83f71ef9730e90d5ea2b9961c4fb90
Apratim Mukherjee <apratim101@gmail.com>
2020-03-26 14:19:01 +0530

    Script to extract frames from a video

---
+++ extract_frames.py
@@ -0,0 +1,23 @@
+import cv2
+
+from params import save_path, vid_path
+
+cap = cv2.VideoCapture(vid_path)
+
+f_id = 0
+every_n_frames = 1
+k = 1
+
+while True:
+    ret, img = cap.read()
+    img_h, img_w, _ = img.shape
+    f_id += 1
+
+    if f_id % every_n_frames == 0:
+        cv2.imwrite(save_path + str(k) + '.png', img)
+
+        cv2.imshow("img", cv2.resize(img, (img_w // 2, img_h // 2)))
+        cv2.waitKey(1)
+
+        print('image' + str(k))
+        k += 1
